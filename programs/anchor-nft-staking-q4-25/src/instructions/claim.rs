c2b1ae8adde7fed0803a1555052fef57cea8e34e
ChubbyChipmunk77 <technozap5002@gmail.com>
2026-02-01 08:25:15 +0530

    did the stake and unstake program , with the claim program. Faced some issues while testing so also added the metaplex mainnet program

--- programs/anchor-nft-staking-q4-25/src/instructions/claim.rs
+++ programs/anchor-nft-staking-q4-25/src/instructions/claim.rs
@@ -6,13 +6,69 @@ use anchor_spl::{
 
 use crate::state::{StakeConfig, UserAccount};
 
-// #[derive(Accounts)]
-// pub struct Claim<'info> {
-// //TODO
-// }
+#[derive(Accounts)]
+pub struct Claim<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-// impl<'info> Claim<'info> {
-//     pub fn claim(&mut self) -> Result<()> {
-//     //TODO
-//     }
-// }
+    #[account(
+        mut,
+        seeds = [b"user", user.key().as_ref()],
+        bump = user_account.bump,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"rewards", config.key().as_ref()],
+        bump = config.rewards_bump,
+        mint::authority = config,
+    )]
+    pub reward_mint: Account<'info, Mint>,
+
+    #[account(
+        seeds = [b"config"],
+        bump = config.bump,
+    )]
+    pub config: Account<'info, StakeConfig>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = reward_mint,
+        associated_token::authority = user,
+    )]
+    pub rewards_account: Account<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> Claim<'info> {
+    pub fn claim(&mut self) -> Result<()> {
+        let points = self.user_account.points;
+
+        if points > 0 {
+            let seeds = &[b"config".as_ref(), &[self.config.bump]];
+            let signer_seeds = &[&seeds[..]];
+
+            let cpi_program = self.token_program.to_account_info();
+            let cpi_accounts = MintTo {
+                mint: self.reward_mint.to_account_info(),
+                to: self.rewards_account.to_account_info(),
+                authority: self.config.to_account_info(),
+            };
+            let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+            mint_to(
+                cpi_ctx,
+                points as u64 * 10_u64.pow(self.reward_mint.decimals as u32),
+            )?;
+
+            self.user_account.points = 0;
+        }
+
+        Ok(())
+    }
+}
